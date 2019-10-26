abff9ddaabae1c317d1e7d0cc2bc301b147551ad
cyan server <cyancoder2016@gmail.com>
2019-10-26 16:21:51 +0330

    fixed relation bug

--- app/Http/Controllers/API/Client/v1/AuthController.php
+++ app/Http/Controllers/API/Client/v1/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
         return response()->json([
             'token'   => $token,
             'type'    => 'bearer',
-            'expires' => auth('api')->factory()->getTTL() * 60, // time to expiration
+            'expires' => auth('api')->factory()->getTTL() * 60 * 300, // time to expiration
         ]);
     }
 
--- app/PersonTiming.php
+++ app/PersonTiming.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class PersonTiming extends Model
 {
-    //
+    protected $fillable=[
+        'person_id',
+        'title',
+        'description',
+        'date',
+        'start',
+        'end',
+        'type',
+        'state',
+        'created_by',
+        'updated_by',
+        'deleted_on',
+    ];
 }
