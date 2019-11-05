6fd5e5e35c99aa09bdc8d327d2ae285aed61ea7b
cyancoserver <cyancoder2016@gmail.com>
2019-11-05 18:55:18 +0330

    Add app config to not show variables in error

--- config/app.php
+++ config/app.php
@@ -122,7 +122,11 @@ return [
     'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
-
+    'debug_blacklist' => [
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),
+    ],
     /*
     |--------------------------------------------------------------------------
     | Autoloaded Service Providers
