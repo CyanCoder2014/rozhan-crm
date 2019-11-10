013fcd4aa04724e86c81b9463732e549abfe0aba
cyan server <cyancoder2016@gmail.com>
2019-11-10 17:12:04 +0330

    farid changed

---
+++ app/Http/Controllers/ProductCategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\ProductCategory;
+use App\Repositories\AppRepositoryImpl;
+
+class ProductCategoryController extends BaseAPIController
+{
+    public function __construct(AppRepositoryImpl $appRepository)
+    {
+        $this->appRepository = $appRepository;
+        $this->model = new ProductCategory();
+    }
+
+    public function store()
+    {
+        \request()->request->add(['created_by' => auth()->id()]);
+        return parent::store();
+    }
+    public function update($id)
+    {
+        \request()->request->add(['updated_by' => auth()->id()]);
+        return parent::update($id);
+    }
+    protected function validationRules()
+    {
+        return [
+//            'parent_id'=>['nullable','exists:product_categories,id'],
+            'title'=>['string','required'],
+//            'image'=>['image'],
+//            'description'=>[],
+//            'number'=>[],
+//            'star'=>[],
+//            'type'=>[],
+//            'state'=>[],
+        ];
+    }
+}
--- app/ProductCategory.php
+++ app/ProductCategory.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductCategory extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'product_category_id',
+        'title',
+        'image',
+        'description',
+        'unit',
+        'initial_amount',
+        'remaining_number',
+        'blocked_number',
+        'price',
+        'predicted_price',
+        'tax',
+        'star',
+        'type',
+        'state',
+        'created_by',
+        'updated_by',
+        'deleted_at',
+    ];
 }
