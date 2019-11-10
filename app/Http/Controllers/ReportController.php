6a27e31d84d5de20e090b83424aca8b09879688b
cyan server <cyancoder2016@gmail.com>
2019-11-10 17:43:40 +0330

    farid changed

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -17,6 +17,77 @@ class ReportController extends Controller
      *
      * report all income categories by date and user and service
      */
+
+    public function reportBase(RepotRequest $request){
+
+        $groupBy=[];
+        switch (request('filter')){
+            case 'service':
+                $groupBy[]='service_id';
+                break;
+            case 'user':
+                $groupBy[]='user_id';
+                break;
+            case 'person':
+                $groupBy[]='person_id';
+                break;
+        }
+        switch (request('time')){
+            case 'y':
+                $groupBy[]=DB::raw('YEAR(order_services.created_at)');
+                break;
+            case 'm':
+                $groupBy[]=DB::raw('MONTH(order_services.created_at)');
+                break;
+            case 'w':
+                $groupBy[]=DB::raw('WEEK(order_services.created_at)');
+                break;
+            case 'd':
+                $groupBy[]=DB::raw('order_services.created_at');
+                break;
+            default:
+                if(count($groupBy) == 0)
+                    $groupBy[]=DB::raw('order_services.created_at');
+                break;
+        }
+        switch (request('order')){
+            case 'price':
+                $order='total';
+                break;
+            default:
+                $order='order_services.created_at';
+                break;
+        }
+        switch (request('sort')){
+            case 'inc':
+                $order_sort='inc';
+                break;
+            default:
+                $order_sort='desc';
+                break;
+        }
+
+        $query = OrderService::
+        select('order_services.service_id','orders.user_id','order_services.person_id',
+            DB::raw('order_services.created_at'),'user_id',
+            DB::raw('SUM(order_services.price) as total'),
+            DB::raw('WEEK(order_services.created_at) as week'))
+            ->where('orders.created_at','>=',to_georgian_date(\request('date_from')))
+            ->where('orders.created_at','<',to_georgian_date(\request('date_to')))
+            ->where('orders.state',Order::created_status)
+            ->join('orders','orders.id','order_services.order_id')
+            ->groupBy($groupBy)
+            ->orderBy($order,$order_sort);
+        if (isset($request->limit) && ctype_digit($request->limit))
+            $query->limit($request->limit);
+        $query = $query->get();
+
+        return $query;
+
+    }
+
+
+
     public function report1(RepotRequest $request){
 
         $groupBy=[];
@@ -141,6 +212,12 @@ class ReportController extends Controller
 
 
     }
+
+
+
+
+
+
     public function UserReport(UserReportRequest $request){
         $groupBy=[];
         switch (request('filter')){
