1b69e5176727ff969d6759059ff58c5710a6c260
cyancoserver <cyancoder2016@gmail.com>
2019-12-09 18:07:16 +0330

    add generate random character function to helper

--- app/helper/helper.php
+++ app/helper/helper.php
@@ -1151,7 +1151,14 @@ function format_time($time)
 }
 
 /* end date function */
-
+function generateRandomString($length = 10,$characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 
 ?>
